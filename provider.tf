7a4906f680f21732800a4f3c64212df24f539479
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-02-26 12:30:39 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "aws"  {
+  region = "eu-west-3"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
